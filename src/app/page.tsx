ca2e841dddc420531226867dae94b518160d225c
Victor Alvar <victor.alvar@tutanota.com>
2024-05-19 14:41:00 -0300

    Add prepare prod version.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
-import Image from 'next/image'
+import {sortPosts, allCoreContent, coreContent} from 'pliny/utils/contentlayer'
+import {allBlogs} from 'contentlayer/generated'
+import {Authors, allAuthors} from 'contentlayer/generated'
+import {MDXLayoutRenderer} from 'pliny/mdx-components'
+import AuthorLayout from '@/layouts/AuthorLayout'
+import Main from './Main'
+import Intro from './Intro'
 
-export default function Home() {
+export default async function Page() {
+  const author = allAuthors.find((p) => p.slug === 'default') as Authors
+  const mainContent = coreContent(author)
+
+  const sortedPosts = sortPosts(allBlogs)
+  const posts = allCoreContent(sortedPosts)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <h1>Hello World!</h1>
-      </div>
-    </main>
+    <>
+      <AuthorLayout content={mainContent}>
+        <MDXLayoutRenderer code={author.body.code} />
+      </AuthorLayout>
+      <Main posts={posts} />
+    </>
   )
 }
